e4bf587b40b51d6802d4538e0d60ed86b9e5b981
Vladimir Glinskih <vladimir.v.glinskikh@gmail.com>
2021-10-18 21:11:36 +0600

    Start project

---
+++ src/test/java/kz/zhelezyaka/springsecuritydemo/SpringSecurityDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package kz.zhelezyaka.springsecuritydemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
