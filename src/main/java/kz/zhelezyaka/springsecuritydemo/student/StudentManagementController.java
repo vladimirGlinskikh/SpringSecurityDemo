2151918f78250ccb3dc1d6fd8398052a831b9fdf
Vladimir Glinskih <vladimir.v.glinskikh@gmail.com>
2021-10-21 00:47:15 +0600

    Build management API

---
+++ src/main/java/kz/zhelezyaka/springsecuritydemo/student/StudentManagementController.java
@@ -0,0 +1,38 @@
+package kz.zhelezyaka.springsecuritydemo.student;
+
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
+@RequestMapping("management/api/v1/students")
+public class StudentManagementController {
+    private static final List<Student> STUDENTS = Arrays.asList(
+            new Student(1, "Vladimir"),
+            new Student(2, "Svetlana"),
+            new Student(3, "Nikolay"),
+            new Student(4, "Luba"),
+            new Student(5, "Olya")
+    );
+
+    @GetMapping
+    public List<Student> getAllStudents() {
+        return STUDENTS;
+    }
+
+    @PostMapping
+    public void registerNewStudent(@RequestBody Student student) {
+        System.out.println(student);
+    }
+
+    @DeleteMapping(path = "{studentId}")
+    public void deleteStudent(@PathVariable("studentId") Integer studentId) {
+        System.out.println(studentId);
+    }
+
+    @PutMapping(path = "{studentId}")
+    public void updateStudent(@PathVariable("studentId") Integer studentId, @RequestBody Student student){
+        System.out.println(String.format("%s %s", studentId, student));
+    }
+}
