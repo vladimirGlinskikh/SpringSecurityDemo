3f37bb1ef15b135181998018755d69d9bae05bbd
Vladimir Glinskih <vladimir.v.glinskikh@gmail.com>
2021-10-21 06:48:09 +0600

    Has authority

--- src/main/java/kz/zhelezyaka/springsecuritydemo/security/ApplicationSecurityConfig.java
+++ src/main/java/kz/zhelezyaka/springsecuritydemo/security/ApplicationSecurityConfig.java
@@ -3,6 +3,7 @@ package kz.zhelezyaka.springsecuritydemo.security;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -12,6 +13,7 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
+import static kz.zhelezyaka.springsecuritydemo.security.ApplicationUserPermission.COURSE_WRITE;
 import static kz.zhelezyaka.springsecuritydemo.security.ApplicationUserRole.*;
 
 @Configuration
@@ -29,9 +31,12 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
         http
                 .csrf().disable()
                 .authorizeRequests()
-                .antMatchers("/", "index", "/css/*", "/js/*")
-                .permitAll()
+                .antMatchers("/", "index", "/css/*", "/js/*").permitAll()
                 .antMatchers("/api/**").hasRole(STUDENT.name())
+                .antMatchers(HttpMethod.DELETE,"/management/api/**").hasAuthority(COURSE_WRITE.name())
+                .antMatchers(HttpMethod.POST,"/management/api/**").hasAuthority(COURSE_WRITE.name())
+                .antMatchers(HttpMethod.PUT,"/management/api/**").hasAuthority(COURSE_WRITE.name())
+                .antMatchers(HttpMethod.GET,"/management/api/**").hasAnyRole(ADMIN.name(), ADMINTRAINEE.name())
                 .anyRequest()
                 .authenticated()
                 .and()
