bc05b66fadb7914a9b21e6cf0c2896737d24c547
Vladimir Glinskih <vladimir.v.glinskikh@gmail.com>
2021-10-26 21:15:07 +0600

    preAuthorise in action

--- src/main/java/kz/zhelezyaka/springsecuritydemo/security/ApplicationUserPermission.java
+++ src/main/java/kz/zhelezyaka/springsecuritydemo/security/ApplicationUserPermission.java
@@ -2,7 +2,7 @@ package kz.zhelezyaka.springsecuritydemo.security;
 
 public enum ApplicationUserPermission {
     STUDENT_READ("student:read"),
-    STUDENT_WRITE("student_write"),
+    STUDENT_WRITE("student:write"),
     COURSE_READ("course:read"),
     COURSE_WRITE("course:write");
 
