008b5dd5d292d1a09ccf858b5b3467436749390c
Vladimir Glinskih <vladimir.v.glinskikh@gmail.com>
2021-10-27 19:56:14 +0600

    Custom Login Page

--- src/main/java/kz/zhelezyaka/springsecuritydemo/security/ApplicationSecurityConfig.java
+++ src/main/java/kz/zhelezyaka/springsecuritydemo/security/ApplicationSecurityConfig.java
@@ -38,7 +38,8 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
                 .anyRequest()
                 .authenticated()
                 .and()
-                .formLogin();
+                .formLogin()
+                .loginPage("/login").permitAll();
     }
 
     @Override
